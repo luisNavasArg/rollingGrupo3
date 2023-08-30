88bf19ee57ce2536899268632f2d04d9fee7220a
nanosp7 <spectormariano1@gmail.com>
2023-08-29 22:07:51 -0300

    works.

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_CNN);
-        console.log("Estamos conectados a la base de  datos");
+        console.log("Estamos conectados a la base de datos");
     } catch (error) {
         throw new Error("Error a la hora de iniciar la base de datos");
     }
